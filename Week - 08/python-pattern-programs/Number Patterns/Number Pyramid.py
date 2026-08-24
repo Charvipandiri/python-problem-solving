f9f0746c1656cdb4415558d84a0eaed975110195
Pandiri Charvi <152405250+Charvipandiri@users.noreply.github.com>
2026-08-24 21:39:28 +0530

    Number Pyramid.py

--- Week - 08/python-pattern-programs/Number Patterns/Number Pyramid.py
+++ Week - 08/python-pattern-programs/Number Patterns/Number Pyramid.py
@@ -2,9 +2,8 @@ n = int(input())
 num = 1
 for i in range(n):
   for j in range(i,n):
-    print(" ", end = "")
+    print("", end = " ")
   for j in range(i+1):
-    print(j+1, end = "")
-  for j in range(i, 0, -1):
-    print(j, end="")
+    print(num, end = " ")
+    num+=1
   print()
